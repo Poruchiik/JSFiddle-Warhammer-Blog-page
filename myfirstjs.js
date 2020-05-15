6da57be61e1b252220bec10e0ca193620b291107
Nologor <fraser.turbet@gmail.com>
2020-05-15 14:55:45 +1000

    added js script to index.html

---
+++ myfirstjs.js
@@ -0,0 +1,15 @@
+var favGod = prompt("What is your favourite Chaos God?");
+
+var nameEl = document.getElementById('favGod');
+nameEl.innerHTML = "All will follow " + favGod "!";
+
+/*var ageEl = document.getElementById('ageCalc');
+
+function ageCalculator() {
+    var birthyear = prompt("What year were you born?");
+    var age = 2020 - birthyear;
+
+    ageEl.innerHTML = "Your age is " + age.toString();
+}
+
+ageEl.addEventListener('click', ageCalculator); */
